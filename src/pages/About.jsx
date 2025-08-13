204d00aaaa22ebba3c0fde0db6d67461433c1869
zoirovdev <abboszoirov17@gmail.com>
2025-08-13 15:31:12 +0500

    Add about me text

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,8 +4,20 @@
 
 const About = () => {
   return (
-    <div>
-      Hello
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-[800px] mt-[150px]">
+        <p className="font-medium font-stretch-semi-expanded tracking-wide
+	  border-l border-[#08CB00] p-4">
+	Toshkentlik dasturiy taminotchiman. Kodlashni, desayn qilishti, 
+	loyihalar qurishti yaxshi ko'raman. Har kuni nimadir o'rganyapman. 
+	U dasturlashta bo'sin, hayotda bo'sin farqi yo'q.  Va o'rganganlarimdi 
+	amaliyotga tatbiq qilyapman. Oxirgi o'rgangan narsam "hammasi muhim".
+	Koddagi har bir belgi, dezayndagi har bir komponent, arxitekturadagi har bir bo'lim, 
+	loyihaning har bir qismi, jamoaning har bir azosi muhim va hammasining o'z o'rni bor.
+	To'g'ri bu bir yangi narsa emas, hamma biladi, men ham bilaman. Lekin 
+	endi jiddiy qarashti boshladim.
+        </p>
+      </div>
     </div>
   )
 }
